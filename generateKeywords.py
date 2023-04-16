b40be044c3e40fb5beebef16c0d61e516cdbe223
Elizabeth Yarrow <eyarrow@outlook.com>
2023-04-16 16:32:42 -0700

    Add dynamically generated text file to folder, closes #2

--- generateKeywords.py
+++ generateKeywords.py
@@ -5,10 +5,10 @@ import yake
 
 post = 'description'
 url_to_use = 'https://careers.azenta.com/job/1417/us_payroll_manager'
-employer = 'Chucky Cheese'
+employer = 'Azenta'
 directory = employer
 save_path = '/home/elizabeth/jobs'
-file_name = f"job-description-{employer}.txt"
+
 
 # Scrape html content from the provided web page
 def get_webpage(url: str) -> str:
@@ -40,24 +40,27 @@ def generate_job_description(content_to_parse: str) -> str:
     data_str = data_str + item.get_text(separator='\n')
   return data_str
 
+# Adds a directory set by default to the employer's name.
+# If it already exists it will add files to the existing directory. 
+def generate_directory(save_path: str, directory: str) -> str:
+  path = os.path.join(save_path, directory)
+  try:
+    os.mkdir(path)
+  except:
+    pass
+  return path
+
 # Set the file settings for saving the final file
 def set_file_config(save_path: str, file_name: str) -> str:
   # dynamically set file name here
   # check to make sure the file name does not already exist
   # if it does increment it
+  file_name = f"job-description-{employer}.txt"
   return os.path.join(save_path, file_name)
 
-# Set the location to save the file here
-path = os.path.join(save_path, directory)
-try:
-  os.mkdir(path)
-except:
-  pass
+path = generate_directory(save_path, directory)
 
-
-
-
-complete_name = set_file_config(save_path, file_name)
+path_to_write = set_file_config(path, employer)
 
 raw_page = get_webpage(url_to_use)
 parsed_page = parse_html(raw_page, post)
@@ -66,6 +69,11 @@ keyword = generate_keywords(parsed_page)
 
 file_to_write = "Employer:  " + employer + '\n' + '\n' + "URL: " + url_to_use + '\n' + '\n' + "Job Description: " + job_description +  '\n' +'\n' + "Keywords: " 
 
-# file1 = open(complete_name, "w")
-# file1.write(file_to_write)
-# file1.close
+if os.path.isfile(path_to_write):
+  print("File is already in use. Please rename the existing file and try again.")
+else:
+  file1 = open(path_to_write, "w")
+  file1.write(file_to_write)
+  file1.close
+
+  

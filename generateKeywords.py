ea03a3803b3af91c8a803274ac5dbeefc969d9e0
Elizabeth Yarrow <eyarrow@outlook.com>
2023-04-15 18:05:20 -0700

    Complete basic code to output the complete job description

--- generateKeywords.py
+++ generateKeywords.py
@@ -10,8 +10,6 @@ def getWebpage(url: str) -> str:
 # parse HTML and remove tags that occurs within the given CSS class
 def parseHTML(contentToParse: str, classToUse: str) -> str:
   soup = BeautifulSoup(contentToParse, 'html.parser')
-  # classSearch = "_class" + "=" + classToUse
-  # print(f"Class search is {classSearch}.")
   data_str = ""
   for item in soup.find_all(class_=classToUse):
     data_str = data_str + item.get_text() + " "
@@ -24,12 +22,19 @@ def generateKeywords(parsedPage: str) -> str:
   for word in keywords:
     print(word)
 
-# Saves a 'preetified' version of the whole job description
+# Saves a 'preetified' version of the whole job description, which only pulls from content
+# in paragraph tags and headers
 def generateJobDescription(contentToParse: str) -> str:
-  
+  soup = BeautifulSoup(contentToParse, 'html.parser')
+  data_str = ""
+  for item in soup.find_all(['p', 'h1', 'h2']):
+    data_str = data_str + item.get_text(separator='\n')
+  return data_str
 
 post = 'description'
 urlToUse = 'https://careers.azenta.com/job/1417/us_payroll_manager'
 rawPage = getWebpage(urlToUse)
 parsedPage = parseHTML(rawPage, post)
-generateKeywords(parsedPage)
+jobDescription = generateJobDescription(rawPage)
+#generateKeywords(parsedPage)
+print(jobDescription)
